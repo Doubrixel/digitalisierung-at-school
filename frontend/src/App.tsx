543baf6615cadd05551fa9ad94d912f5dd4f1578
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2021-09-05 21:55:47 +0200

    Navigation mit react-router aufgesetzt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter, Switch, Route,
+} from 'react-router-dom';
+import Toolbar from './components/Toolbar/Toolbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Digitalisierung</h1>
-    </div>
+    <BrowserRouter>
+      <Toolbar />
+      <Switch>
+        <Route exact path="/"><h1>HOME</h1></Route>
+        <Route exact path="/ag"><h1>AG BUCHUNG</h1></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Toolbar() {
+  return (
+    <div>
+      <Link to="/ag">AG Buchung</Link>
+    </div>
+  );
+}
+
+export default Toolbar;
