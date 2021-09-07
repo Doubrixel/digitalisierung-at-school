f32e9d2fcd9fb0206ec5d61c9cefbf958f1468b3
Ructabo <skruse19@hwr-berlin.de>
2021-09-07 17:07:41 +0200

    Footer added.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Digitalisierung</h1>
+      <Footer />
+      <h1> Below Footer? </h1>
     </div>
   );
 }
