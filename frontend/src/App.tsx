23291bd50540b27a4c59bb876196705832ef26bb
Jojo2412 <johannakoehler99@gmx.de>
2021-09-09 11:19:40 +0200

    prepared for merging

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import { TextFormInputFifthExamComponent } from './components/forminputElementText';
-import { RadioButtonFormInputFifthExamComponent } from './components/forminputElementRadioButton';
-import { SubmitButtonFormInputFifthExamComponent } from './components/forminputElementSubmitButton';
-
 function App() {
   return (
-    <form>
-      <TextFormInputFifthExamComponent />
-      <RadioButtonFormInputFifthExamComponent />
-      <SubmitButtonFormInputFifthExamComponent />
-    </form>
+    <div />
   );
 }
 
