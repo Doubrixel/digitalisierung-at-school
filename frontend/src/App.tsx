ef2ca8913b0c7a4fb605a14927fe3ec52873979a
Ructabo <skruse19@hwr-berlin.de>
2021-09-09 11:50:06 +0200

    Horizontal line corrected, App.tsx back to original state.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Digitalisierung</h1>
-      <Footer />
-      <h1> Below Footer? </h1>
     </div>
   );
 }
