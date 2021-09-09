a8b6e19f06fabbee3f22165510d2c6221f5be874
Jojo2412 <johannakoehler99@gmx.de>
2021-09-09 21:30:27 +0200

    forminput props overhaul, central colours

--- frontend/src/components/FormInput/forminputElementRadioButton.tsx
+++ frontend/src/components/FormInput/forminputElementRadioButton.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import './forminputElementRadioButton.css';
+import PropTypes from 'prop-types';
 
-function RadioButtonFormInputFifthExamComponent() {
+function RadioButtonFormInputFifthExamComponent(
+  {
+    labelText, name, maxLabelWidth, minLabelWidth,
+  }:
+  { labelText:string, name:string, maxLabelWidth:string, minLabelWidth:string },
+) {
   return (
     <div className="radioButtonInputFormElement">
       <label htmlFor="theInput">
-        <div className="labelTextDiv">Präsentationsprüfung:</div>
-        <div className="inputDivRadioElement"><input type="radio" id="theInput" className="radioButtonInput" /></div>
+        <div className="labelTextDiv" style={{ maxWidth: maxLabelWidth, minWidth: minLabelWidth }}>{labelText}</div>
+        <div className="inputDivRadioElement"><input type="radio" name={name} className="radioButtonInput" id="theInput" /></div>
       </label>
     </div>
   );
 }
+
+RadioButtonFormInputFifthExamComponent.propTypes = {
+  labelText: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  maxLabelWidth: PropTypes.string,
+  minLabelWidth: PropTypes.string,
+};
+
+RadioButtonFormInputFifthExamComponent.defaultProps = {
+  maxLabelWidth: '500px',
+  minLabelWidth: '200px',
+};
+
 export default RadioButtonFormInputFifthExamComponent;
--- frontend/src/components/FormInput/forminputElementText.tsx
+++ frontend/src/components/FormInput/forminputElementText.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import './forminputElementText.css';
+import PropTypes from 'prop-types';
 
-function TextFormInputFifthExamComponent() {
+function TextFormInputFifthExamComponent(
+  {
+    labelText, name, maxLabelWidth, minLabelWidth,
+  }:
+  { labelText:string, name:string, maxLabelWidth:string, minLabelWidth:string },
+) {
   return (
     <div className="textInputFormElement">
       <label htmlFor="theInput">
-        <div className="labelTextDiv">Prüfling:</div>
-        <div className="inputDivTextElement"><input type="text" id="theInput" className="textInput" /></div>
+        <div className="labelTextDiv" style={{ maxWidth: maxLabelWidth, minWidth: minLabelWidth }}>{labelText}</div>
+        <div className="inputDivTextElement"><input type="text" name={name} className="textInput" id="theInput" /></div>
       </label>
     </div>
   );
 }
+
+TextFormInputFifthExamComponent.propTypes = {
+  labelText: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  maxLabelWidth: PropTypes.string,
+  minLabelWidth: PropTypes.string,
+};
+
+TextFormInputFifthExamComponent.defaultProps = {
+  maxLabelWidth: '500px',
+  minLabelWidth: '200px',
+};
 
 export default TextFormInputFifthExamComponent;
