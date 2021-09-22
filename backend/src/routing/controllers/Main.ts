616527c56972780a08d7be082dd3ad4c9db889fe
bzenz <s_zenz19@stud.hwr-berlin.de>
2021-09-22 16:21:53 +0200

    Backend client serving

--- backend/src/routing/controllers/Main.ts
+++ backend/src/routing/controllers/Main.ts
@@ -1,7 +1,12 @@
-import { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
+import path from 'path';
+
+const client = path.join(__dirname, '/../../../client/');
+const clientIndex = path.join(client, 'index.html');
 
 export default class Main {
     static GEThomepage(req: Request, res: Response): void {
-        res.send('Homepage');
+        res.sendFile(clientIndex);
     }
+    static clientfiles = express.static(client);
 }
--- backend/src/routing/routes/index.ts
+++ backend/src/routing/routes/index.ts
@@ -4,7 +4,11 @@ import Main from '../controllers/Main';
 export default (): Router => {
     const router = Router();
 
-    router.get('/', Main.GEThomepage);
+    // Host client files
+    router.use(Main.clientfiles);
+
+    // Client side routing => always provide index.html
+    router.get('*', Main.GEThomepage);
 
     return router;
 };
