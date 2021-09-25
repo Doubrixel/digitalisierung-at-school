801744471a0fe527540941492d8f34af9ec440e3
Skyrorax <skyrorax@users.noreply.github.com>
2021-09-25 13:29:26 +0200

    added text to numbers.test.js

--- templates/jestMatchers/numbers/numbers.test.js
+++ templates/jestMatchers/numbers/numbers.test.js
@@ -12,6 +12,6 @@ test('two plus two', () => {
 
 test('adding floating point numbers', () => {
   const value = 0.1 + 0.2;
-  expect(value).toBe(0.3);        // This won't work because of rounding error
+  expect(value).toBe(0.3);       // This won't work because of rounding error
   expect(value).toBeCloseTo(0.3); // This works.
 });
