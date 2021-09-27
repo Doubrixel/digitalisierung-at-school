92cb4913973342534aeb46db7c0aef574ebc4862
Fillebiep <77406590+Fillebiep@users.noreply.github.com>
2021-09-27 09:58:01 +0200

    Update async.test.js

---
+++ templates/asynchronousCode/async/async.test.js
@@ -0,0 +1,21 @@
+test('the data is peanut butter', async () => {
+  const data = await fetchData();
+  expect(data).toBe('peanut butter');
+});
+
+test('the fetch fails with an error', async () => {
+  expect.assertions(1);
+  try {
+    await fetchData();
+  } catch (e) {
+    expect(e).toMatch('error');
+  }
+});
+
+test('the data is peanut butter', async () => {
+  await expect(fetchData()).resolves.toBe('peanut butter');
+});
+
+test('the fetch fails with an error', async () => {
+  await expect(fetchData()).rejects.toMatch('error');
+});
