8e78b6fdfa7f9d0c9699b80a8e2a8512a58fd1c2
Huebner Jan <Jan.Huebner@data-experts.de>
2021-09-28 09:24:30 +0200

    design der einzelnen Kopmponenten responsiv angepasst
    - material ui icons installiert

--- frontend/src/pages/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage.tsx
@@ -3,10 +3,50 @@
  */
 
 import React from 'react';
+import SettingsModuleComponent from '../components/SettingsModuleComponent/SettingsModuleComponent';
+import AGImage from '../images/AG.png';
+import FacharbeitImage from '../images/Facharbeit.png';
+import WahlpflichtImage from '../images/Wahlpflicht.png';
+import PruefungskompoenenteImage from '../images/PK.png';
+
+const modules = [
+  {
+    title: 'AG-Buchung',
+    image: AGImage,
+  },
+  {
+    title: 'Facharbeit',
+    image: FacharbeitImage,
+  },
+  {
+    title: 'Wahlpflicht',
+    image: WahlpflichtImage,
+  },
+  {
+    title: '5. Prüfungskomponente',
+    image: PruefungskompoenenteImage,
+  },
+];
 
 function SettingsPage() {
   return (
-    <h1>Einstellungen</h1>
+    <div>
+      <div className="flexboxStart">
+        <h1 id="HeadlineStart">Einstellungen</h1>
+      </div>
+      <div className="flexboxModul">
+        {
+        modules.map((module) => (
+          <div className="flexModulItem">
+            <SettingsModuleComponent
+              title={module.title}
+              image={module.image}
+            />
+          </div>
+        ))
+        }
+      </div>
+    </div>
   );
 }
 
