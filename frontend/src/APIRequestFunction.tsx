0c402019d964de2d10ffee5b48c863ea7bdeac21
Robin Hoentsch <robin.hoentsch@data-experts.de>
2021-11-19 14:59:16 +0100

    Review bearbeitet 2

--- frontend/src/APIRequestFunction.tsx
+++ frontend/src/APIRequestFunction.tsx
@@ -1,16 +1,29 @@
+/*
+To use the sendAPIRequest function, you need to pass the api-path,
+requestMethod and requestBody (if the requestMethod isnt GET)
+
+To extract the response and save it in a reactHookuse, use the following syntax:
+sendAPIRequest('api/abitur/test', 'GET').then((response) => (setDataHook(reponse)));
+
+But note, that the sendAPIRequest-function returns a promise, which isn't immediately resolved
+ */
+
+import path from 'path';
 import store from './store';
 
+export const SERVER = window.location.origin === 'http://localhost:3000' ? 'localhost:5000' : '';
 export default function sendAPIRequest(reqPath: string, reqMethod: string, reqBody?: any) {
   const { authToken } = store.getState().authReducer;
   const reqHeaders = new Headers();
   reqHeaders.append('Content-Type', 'application/json');
   reqHeaders.append('Authorization', authToken);
-  fetch((reqPath),
+  const finalreqpath = `http://${path.join(SERVER, reqPath)}`;
+  return fetch((finalreqpath),
     {
       method: reqMethod,
       headers: reqHeaders,
       body: reqBody,
     })
     .then((response) => response.json())
-    .then((data) => (data));
+    .then((data) => data);
 }
