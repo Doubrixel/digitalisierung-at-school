2a7cda699f24674fb039b133819fd43315d35c9f
BeeIBerry <ericberlin24@gmail.com>
2021-11-21 14:08:31 +0100

    Weiterleitungs-Link aus .env-Datei lesen

--- backend/src/auth/staticAuthStrings.ts
+++ backend/src/auth/staticAuthStrings.ts
@@ -1,6 +1,5 @@
 // Links
 export const iservLink = 'https://kant-gymnasium.de/iserv/public';
-export const redirectLink = 'http://einschreibung.kant-gymnasium.de:5000/auth/callback';
 
 // Errors
 export const iservConnectToLoginError = 'An error occurred while trying to connect to the iserv login page';
