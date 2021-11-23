8b529509a3541ec8b51c2ad9dab19d6ddc772f88
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2021-11-23 09:46:42 +0100

    kommis entfernt

--- frontend/src/reducer/index.tsx
+++ frontend/src/reducer/index.tsx
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux';
 import authReducer, { AuthState } from './authReducer';
-// eslint-disable-next-line import/no-cycle
-// import store from '../store';
 
 const rootReducer = combineReducers({
   authReducer,
