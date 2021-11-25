bb80fa21f9f40ceb664f1a3634ab2cbfab5303b6
Wilhelm-kaden <70752464+Wilhelm-kaden@users.noreply.github.com>
2021-11-25 09:39:18 +0100

    UebersichtAdminFA

--- frontend/src/pages/admin/AdminFacharbeitPage.tsx
+++ frontend/src/pages/admin/AdminFacharbeitPage.tsx
@@ -3,11 +3,35 @@
  */
 
 import React from 'react';
+import './AdminFacharbeitPage.css';
+import ComponentToggleButton from '../../components/Buttons/ComponentToggleButton';
+import TableUebersichtFA from '../../components/ReusableComponents/TableUebersichtFA';
+import CreatePDFButton from '../../components/Buttons/CreatePDFButton';
 
 function AdminFacharbeitPage() {
+  const handleGeneratePDF = () => {
+
+  };
+  const handleComponentActivation = () => {
+
+  };
   return (
-    <div>
-      Bald verfügbar!
+    <div className="GridAdminUebersichtFA">
+      <p id="HeadlineFA">Facharbeit Übersicht</p>
+      <span
+        id="ToogleButtonFA"
+        style={{
+          marginRight: '0', marginLeft: 'auto', width: '50vw', maxWidth: '320px',
+        }}
+      >
+        <ComponentToggleButton componentLabel="Komponente deaktivieren" buttonHandleClickFunction={handleComponentActivation} />
+      </span>
+      <div id="TableUebersichtFA">
+        <TableUebersichtFA />
+      </div>
+      <div id="ButtonUebersichtFA">
+        <CreatePDFButton style={{ marginTop: '3vh' }} onClick={handleGeneratePDF} />
+      </div>
     </div>
   );
 }
