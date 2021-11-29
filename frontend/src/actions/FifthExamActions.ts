e4127726783e49d4d2272eb9fa66614e31b1823a
Robin <robin0507@gmx.de>
2021-11-29 12:06:41 +0100

    5PK Admin Formularbearbeitung umgesetzt

---
+++ frontend/src/actions/FifthExamActions.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
+export function setPreFilledDataIn5PKFormEditedByAdmin(prefilledData) {
+  return {
+    type: 'SET_PREFILLED_DATA',
+    prefilledData,
+  };
+}
