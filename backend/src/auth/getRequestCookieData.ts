bcbd656c7aebdf428525514b209a5d84402aa480
BeeIBerry <ericberlin24@gmail.com>
2021-11-29 12:08:10 +0100

    getStudentId implementiert
    - Admin Test-Schnittstelle hinzugefügt

--- backend/src/auth/getRequestCookieData.ts
+++ backend/src/auth/getRequestCookieData.ts
@@ -1,23 +1,9 @@
 import {createUser} from './sso';
-import {Request, Response} from 'express';
-import {getFirstResult} from '../db/dbAccessor';
+import {Request} from 'express';
 
-const getStudentId = async function (req : Request, res : Response): Promise<number> {
+const getStudentId = function (req : Request): string {
     const user = await createUser(req.session?.user);
-    return new Promise<number>(resolve => {
-        const sql = 'SELECT id FROM nutzer WHERE iServUuid = ?';
-        getFirstResult(sql, [user.uuid], (obj, err) => {
-            if (err) {
-                res.status(500).json(err.message);
-            } else {
-                if (!obj) res.status(500).json('Nutzer nicht bekannt');
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                else { // @ts-ignore
-                    resolve(obj.id);
-                }
-            }
-        });
-    });
+    return user.uuid;
 };
 
 export {getStudentId};
