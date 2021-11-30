cceba1c409c4ebfb1a6b239a056eaef9e4741ca7
Robin <robin0507@gmx.de>
2021-11-30 14:38:14 +0100

    Testcommit für auth/getUserData

--- frontend/src/actions/authActions.tsx
+++ frontend/src/actions/authActions.tsx
@@ -1,2 +1,7 @@
 export const login = () => ({ type: 'LOGIN' });
 export const logout = () => ({ type: 'LOGOUT' });
+export const setUserData = (userName, role) => ({
+  type: 'SET_USER_DATA',
+  userName,
+  role,
+});
--- frontend/src/reducer/authReducer.tsx
+++ frontend/src/reducer/authReducer.tsx
@@ -1,6 +1,7 @@
 export interface AuthState {
   isLoggedIn: boolean,
   authToken: string,
+  userName: string,
   role: string,
   accessibleComponents: string[],
 }
@@ -8,14 +9,10 @@ export interface AuthState {
 const initialState: AuthState = {
   isLoggedIn: false,
   authToken: '',
+  userName: '',
   role: '',
   accessibleComponents: [],
 };
-
-// funktionen erstmal nur gemockt
-function getRoleOfUser() {
-  return 'admin';
-}
 
 // funktionen erstmal nur gemockt
 function getAccessibleComponentsOfUser() {
@@ -26,11 +23,15 @@ const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'LOGIN':
       return {
-        ...state, isLoggedIn: true, authToken: 'authTokenMock', role: getRoleOfUser(), accessibleComponents: getAccessibleComponentsOfUser(),
+        ...state, isLoggedIn: true, authToken: 'authTokenMock', accessibleComponents: getAccessibleComponentsOfUser(),
       };
     case 'LOGOUT':
       return {
         ...state, isLoggedIn: false, authToken: null, role: null, accessibleComponents: [],
+      };
+    case 'SET_USER_DATA':
+      return {
+        ...state, userName: action.userName, role: action.role,
       };
     default:
       return state;
