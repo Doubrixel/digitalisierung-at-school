e10f48054a19851b6aadae074a347cd67918d60c
bzenz <s_zenz19@stud.hwr-berlin.de>
2021-12-01 16:52:38 +0100

    Schnittstelle api/facharbeit/uploadDocument (POST)
    Schnittstelle umgesetzt.
    Fehlformatierte Testdaten entfernt.

---
+++ backend/migrations/sqls/20211201152520-facharbeit-testdaten-down.sql
@@ -0,0 +1,2 @@
+UPDATE facharbeiten SET dokumentpfad = "./hier/und/da", abgabezeitpunkt = "2004-08-19 18:51:06" WHERE id IS 1;
+UPDATE facharbeiten SET dokumentpfad = "./dort", abgabezeitpunkt = "2004-09-29 07:04:26" WHERE id IS 2;
--- backend/src/routing/validators/api/facharbeit.ts
+++ backend/src/routing/validators/api/facharbeit.ts
@@ -1,4 +1,9 @@
-import {body, ValidationChain} from 'express-validator';
+import { body, check, ValidationChain } from 'express-validator';
+import * as multer from 'multer';
+const storage = multer.memoryStorage(); // Holds a buffer of the file in memory
+const upload = multer.default({ storage: storage });
+
+const UPLOAD_DOCUMENT_NAME = 'document';
 
 export default class FacharbeitValidators {
     static isOptionalString = (fieldName: string): ValidationChain => body(fieldName).isString().trim().optional();
@@ -7,5 +12,17 @@ export default class FacharbeitValidators {
         FacharbeitValidators.isOptionalString('subject'),
         FacharbeitValidators.isOptionalString('choosenTeacher'),
         FacharbeitValidators.isOptionalString('subjectteacher'),
+    ];
+    static POSTuploadDocument = [
+        // multer middleware
+        upload.single(UPLOAD_DOCUMENT_NAME),
+
+        // express-validator middleware
+        check(UPLOAD_DOCUMENT_NAME)
+        .custom((value, {req}) => {
+                if (req.files[UPLOAD_DOCUMENT_NAME].mimetype === 'application/pdf') return '.pdf';
+                else return false;
+            })
+        .withMessage('Please only submit pdf documents.'), // custom error message that will be send back if the file in not a pdf.
     ];
 }
