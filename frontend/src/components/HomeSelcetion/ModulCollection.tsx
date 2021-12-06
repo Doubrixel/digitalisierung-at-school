40a3614f95bfa473f002164ef412a250f3e92565
Wilhelm-kaden <70752464+Wilhelm-kaden@users.noreply.github.com>
2021-12-06 15:44:59 +0100

    StartseiteNichtangemeldet

--- frontend/src/components/HomeSelcetion/ModulCollection.tsx
+++ frontend/src/components/HomeSelcetion/ModulCollection.tsx
@@ -1,4 +1,3 @@
-/*eslint-disable*/
 import React from 'react';
 import './ModulCollection.css';
 
@@ -22,46 +21,45 @@ function ModulCollection(props) {
   }
 
   return (
-    <>
+    <div>
       {isLoggedIn ? (
-            <div className="flexboxModul">
-              {role === STUDENT_ROLE || role === SUPER_ADMIN_ROLE || role === FA_ADMIN_ROLE
-                ? (
-                  <div className="flexModulItem">
-                    <SelectionComponent
-                      image={Facharbeit_Image}
-                      title="Facharbeit für die SuS der Klasse 9 einreichen"
-                      buttontext="Facharbeit"
-                      path={`/${approprateRolePath}/facharbeit`}
-                    />
-                  </div>
-                ) : null}
-              {role === STUDENT_ROLE || role === SUPER_ADMIN_ROLE || role === FIFTH_PK_ADMIN_ROLE
-                ? (
-                  <div className="flexModulItem">
-                    <SelectionComponent
-                      image={PK_Image}
-                      title="Abitur-Prüfungsabgabe für die Klasse 11 bzw. 12"
-                      buttontext="5. PK"
-                      path={`/${approprateRolePath}/pruefungskomponente`}
-                    />
-                  </div>
-                ) : null}
-            </div>
-          ) : (
-            <div className="flexModulItem">
-              <LogoutAnsicht />
-            </div>
-          )
-      }
-    </>
+        <div className="flexboxModul">
+          {role === STUDENT_ROLE || role === SUPER_ADMIN_ROLE || role === FA_ADMIN_ROLE
+            ? (
+              <div className="flexModulItem">
+                <SelectionComponent
+                  image={Facharbeit_Image}
+                  title="Facharbeit für die SuS der Klasse 9 einreichen"
+                  buttontext="Facharbeit"
+                  path={`/${approprateRolePath}/facharbeit`}
+                />
+              </div>
+            ) : null}
+          {role === STUDENT_ROLE || role === SUPER_ADMIN_ROLE || role === FIFTH_PK_ADMIN_ROLE
+            ? (
+              <div className="flexModulItem">
+                <SelectionComponent
+                  image={PK_Image}
+                  title="Abitur-Prüfungsabgabe für die Klasse 11 bzw. 12"
+                  buttontext="5. PK"
+                  path={`/${approprateRolePath}/pruefungskomponente`}
+                />
+              </div>
+            ) : null}
+        </div>
+      ) : (
+        <div className="flexModulItem">
+          <LogoutAnsicht />
+        </div>
+      )}
+    </div>
   );
 }
 
 function mapStateToProps(state) {
   return {
     role: state.authReducer.role,
-    isLoggedIn: state.authReducer.isLoggedIn
+    isLoggedIn: state.authReducer.isLoggedIn,
   };
 }
 
