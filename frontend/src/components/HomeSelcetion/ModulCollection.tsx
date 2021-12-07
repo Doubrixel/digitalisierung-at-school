987563e2cbe7ae66ef8f7dcba20866173632ba88
Wilhelm-kaden <70752464+Wilhelm-kaden@users.noreply.github.com>
2021-12-07 12:54:25 +0100

    FehlerbehebungModulCollection

--- frontend/src/components/HomeSelcetion/ModulCollection.tsx
+++ frontend/src/components/HomeSelcetion/ModulCollection.tsx
@@ -21,40 +21,40 @@ function ModulCollection(props) {
   }
 
   return (
-  <div>
-    {isLoggedIn ? (
-      <div className="flexboxModul">
-        {/* eslint-disable-next-line max-len */}
-        {(role === STUDENT_ROLE && classNumber === 9) || role === SUPER_ADMIN_ROLE || role === FA_ADMIN_ROLE
-          ? (
-            <div className="flexModulItem">
-              <SelectionComponent
-                image={Facharbeit_Image}
-                title="Facharbeit für die SuS der Klasse 9 einreichen"
-                buttontext="Facharbeit"
-                path={`/${approprateRolePath}/facharbeit`}
-              />
-            </div>
-          ) : null}
-        {/* eslint-disable-next-line max-len */}
-        {(role === STUDENT_ROLE && (classNumber === 11 || classNumber === 12)) || role === SUPER_ADMIN_ROLE || role === FIFTH_PK_ADMIN_ROLE
-          ? (
-            <div className="flexModulItem">
-              <SelectionComponent
-                image={PK_Image}
-                title="Abitur-Prüfungsabgabe für die Klasse 11 bzw. 12"
-                buttontext="5. PK"
-                path={`/${approprateRolePath}/pruefungskomponente`}
-              />
-            </div>
-          ) : null}
-      </div>
+    <div>
+      {isLoggedIn ? (
+        <div className="flexboxModul">
+          {/* eslint-disable-next-line max-len */}
+          {(role === STUDENT_ROLE && classNumber === 9) || role === SUPER_ADMIN_ROLE || role === FA_ADMIN_ROLE
+            ? (
+              <div className="flexModulItem">
+                <SelectionComponent
+                  image={Facharbeit_Image}
+                  title="Facharbeit für die SuS der Klasse 9 einreichen"
+                  buttontext="Facharbeit"
+                  path={`/${approprateRolePath}/facharbeit`}
+                />
+              </div>
+            ) : null}
+          {/* eslint-disable-next-line max-len */}
+          {(role === STUDENT_ROLE && (classNumber === 11 || classNumber === 12)) || role === SUPER_ADMIN_ROLE || role === FIFTH_PK_ADMIN_ROLE
+            ? (
+              <div className="flexModulItem">
+                <SelectionComponent
+                  image={PK_Image}
+                  title="Abitur-Prüfungsabgabe für die Klasse 11 bzw. 12"
+                  buttontext="5. PK"
+                  path={`/${approprateRolePath}/pruefungskomponente`}
+                />
+              </div>
+            ) : null}
+        </div>
       ) : (
         <div className="flexModulItem">
           <LogoutAnsicht />
         </div>
       )}
-  </div>
+    </div>
   );
 }
 
